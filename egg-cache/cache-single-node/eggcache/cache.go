cba20fd96a42db10bfe00a2cd3ba1d064caa6a23
qiuzhihan <qiuzhihan@xinyu668.com>
2020-12-15 11:34:22 +0800

    cache-single-node:添加💉注释

--- egg-cache/cache-single-node/eggcache/cache.go
+++ egg-cache/cache-single-node/eggcache/cache.go
@@ -6,11 +6,12 @@ import (
 )
 
 type cache struct {
-	lru        *lru.Cache
-	cacheBytes int64
-	mu         sync.Mutex
+	lru        *lru.Cache //lru
+	cacheBytes int64      // max size
+	mu         sync.Mutex //并发安全
 }
 
+//save 插入或更新缓存
 func (c *cache) save(key string, value ByteView) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
@@ -20,6 +21,7 @@ func (c *cache) save(key string, value ByteView) {
 	c.lru.Save(key, value)
 }
 
+//get 获取缓存
 func (c *cache) get(key string) (value ByteView, ok bool) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
--- egg-cache/cache-single-node/eggcache/eggcache.go
+++ egg-cache/cache-single-node/eggcache/eggcache.go
@@ -18,7 +18,7 @@ func (f GetterFunc) Get(key string) ([]byte, error) {
 
 type Group struct {
 	name      string
-	getter    Getter
+	getter    Getter //回调
 	mainCache cache
 }
 
@@ -49,6 +49,7 @@ func GetGroup(name string) *Group {
 	return group
 }
 
+//Get 获取缓存
 func (g *Group) Get(key string) (ByteView, error) {
 	if key == "" {
 		return ByteView{}, errors.New("key is required")
@@ -61,11 +62,15 @@ func (g *Group) Get(key string) (ByteView, error) {
 	return g.load(key)
 }
 
+//load
 func (g *Group) load(key string) (ByteView, error) {
 	return g.getLocally(key)
 }
 
+//getLocally 读取本地数据源
 func (g *Group) getLocally(key string) (ByteView, error) {
+	//读取数据源
+	//写法很骚，定义一个函数类型 F，并且实现接口 A 的方法，然后在这个方法中调用自己。这是 Go 语言中将其他函数（参数返回值定义与 F 一致）转换为接口 A 的常用技巧。
 	resource, err := g.getter.Get(key)
 	if err != nil {
 		return ByteView{}, err
