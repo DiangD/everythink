7b35dd53bda5be3298d7b3236e91d9e3e8fa05b9
qiuzhihan <qiuzhihan@xinyu668.com>
2020-12-16 13:57:46 +0800

    cache-multi_node:多节点

---
+++ egg-cache/cache-multi-node/eggcache/http.go
@@ -0,0 +1,109 @@
+package eggcache
+
+import (
+	"eggcache/consistenthash"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+	"strings"
+	"sync"
+)
+
+const (
+	defaultBasePath = "/_eggcache/"
+	defaultReplicas = 50
+)
+
+type HTTPPool struct {
+	self        string
+	basePath    string
+	mu          sync.Mutex
+	peers       *consistenthash.Map
+	httpGetters map[string]*HTTPGetter
+}
+
+type HTTPGetter struct {
+	baseURL string
+}
+
+func (g *HTTPGetter) Get(group, key string) ([]byte, error) {
+	u := fmt.Sprintf("%v%v/%v", g.baseURL, url.QueryEscape(group), url.QueryEscape(key))
+	resp, err := http.Get(u)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("server returned: %v", resp.Status)
+	}
+
+	bytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("reading response body: %v", err)
+	}
+	return bytes, nil
+}
+
+func NewHTTPPool(self string) *HTTPPool {
+	return &HTTPPool{
+		self:     self, //本机的ip+端口
+		basePath: defaultBasePath,
+	}
+}
+
+func (p *HTTPPool) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	if !strings.HasPrefix(req.URL.Path, p.basePath) {
+		panic("HTTPPool serving unexpected path: " + req.URL.Path)
+	}
+	p.Log("%s %s", req.Method, req.URL.Path)
+
+	// 格式：<basePath>/<group_name>/<key>
+	parts := strings.SplitN(req.URL.Path[len(p.basePath):], "/", 2)
+	if len(parts) != 2 {
+		http.Error(w, "bad request", http.StatusBadRequest)
+		return
+	}
+
+	groupName, key := parts[0], parts[1]
+	group := GetGroup(groupName)
+	if group == nil {
+		http.Error(w, "no such group: "+groupName, http.StatusNotFound)
+		return
+	}
+	value, err := group.Get(key)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	//二进制数据流
+	w.Header().Set("Content-Type", "application/octet-stream")
+	_, _ = w.Write(value.ByteSlice())
+}
+
+func (p *HTTPPool) Log(format string, v ...interface{}) {
+	log.Printf("[Server %s] %s", p.self, fmt.Sprintf(format, v...))
+}
+
+func (p *HTTPPool) Set(peers ...string) {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+	p.peers = consistenthash.New(defaultReplicas, nil)
+	p.peers.Add(peers...)
+	p.httpGetters = make(map[string]*HTTPGetter, len(peers))
+	for _, peer := range peers {
+		p.httpGetters[peer] = &HTTPGetter{peer + p.basePath}
+	}
+}
+
+func (p *HTTPPool) PickPeer(key string) (PeerGetter, bool) {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+
+	if peer := p.peers.Get(key); peer != "" && peer != p.self {
+		p.Log("Pick peer %s", peer)
+		return p.httpGetters[peer], true
+	}
+	return nil, false
+}
